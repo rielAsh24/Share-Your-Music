63275e543260f6cfb24a1682a7aa34c980547365
Ashriel <ashriel31@gmail.com>
2025-10-27 11:48:45 +0530

    refactor: moved tests to src

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -8,7 +8,7 @@ loadEnvFile(path.resolve(__dirname, ".env.development"));
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "src/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
